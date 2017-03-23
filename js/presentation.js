0979370f62ca04e7fefe9b3d2883073296942963
md <dmitry.corsa@gmail.com>
2017-03-23 22:51:22 +0300

    Небольшие правки

---
+++ js/presentation.js
@@ -0,0 +1,19 @@
+
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    overview: true,
+    mouseWheel: true,
+    keyboard: true,
+    transition: 'slide', // none/fade/slide/convex/concave/zoom
+    parallaxBackgroundImage: '../../img/topography.png',
+    parallaxBackgroundHorizontal: 200,
+    parallaxBackgroundVertical: 100
+});
+
+window.onload = function() {
+    var reveal = document.getElementsByClassName('reveal')[0];
+    reveal.className += ' active';
+};
